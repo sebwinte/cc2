8b06f686d2346356659d8795c3433849e70412e3
DrWinter <sebastian.winter@nerdindustries.com>
2021-04-14 17:32:33 +0200

    working on multiple compressions flags

--- controller/controller.py
+++ controller/controller.py
@@ -28,12 +28,13 @@ class Controller:
     # process 
 
     def process(self,file_name_without_arguments,arguments,original_file_path,file_name_without_arguments_extension,cropped_file_extension,original_file_name):
-
         verified_arguments_compression = self.verify_arguments_compression(arguments)
-        verified_arguments_type = self.verify_arguments_type(arguments)
-        
+        print("#######")
+        print(verified_arguments_compression)
+        print("#######")
+        verified_arguments_type = self.verify_arguments_type(arguments,cropped_file_extension)
         unique_folder_id = self.dir.make_dir(Helper.path,file_name_without_arguments,0o777)
-        print(unique_folder_id)
+        #print(unique_folder_id)
         if self.conv.manage_filetype_compression(verified_arguments_compression,verified_arguments_type,file_name_without_arguments,original_file_name,unique_folder_id):
             if self.dir.move_files(original_file_name,Helper.path,file_name_without_arguments,unique_folder_id):
                 self.h.notification_message("cc2","Your video has been successfully converted")
@@ -41,7 +42,7 @@ class Controller:
         else: return   
 
 
-    # verify_arguments_compression return only valid arguments according to the Helper.valid_arguments_ list 
+    # verify_arguments_compression return only valid arguments according to the Helper.valid_arguments_compression
 
     def verify_arguments_compression(self,arguments):
         valid_arguments = []
@@ -49,6 +50,10 @@ class Controller:
             for param in arguments:
                 if param.lower() in Helper.valid_arguments_compression:
                     valid_arguments.append(param.lower())
+
+                    if len(valid_arguments) > 1:
+                        print(len(valid_arguments))
+
             # Default if no compression argument found 
             if not valid_arguments:
                 valid_arguments =  ["medium"]
@@ -57,14 +62,17 @@ class Controller:
             return False   
 
 
-    # verify_arguments_type return only valid arguments according to the Helper.valid_arguments_ list 
+    # verify_arguments_type return only valid arguments according to the Helper.valid_arguments_type 
 
-    def verify_arguments_type(self,arguments):
+    def verify_arguments_type(self,arguments,cropped_file_extension):
         valid_arguments = []
         try:
             for param in arguments:
                 if param.lower() in Helper.valid_arguments_type:
                     valid_arguments.append(param.lower())
+            if not valid_arguments:
+                 valid_arguments.append(cropped_file_extension.lower())
+                 print(cropped_file_extension.lower())
             return valid_arguments
         except:
             return False    
